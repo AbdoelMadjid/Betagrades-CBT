5d1561b0d06f70280d5d7fdc17f7290febb3ec6f
gbxnga <iamblizzyy@gmail.com>
2018-09-19 17:37:44 +0100

    db update

--- mysqli.inc.php
+++ mysqli.inc.php
@@ -1,9 +1,19 @@
 <?php
+ 
+if ($_SERVER['SERVER_NAME'] == "localhost"){
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'cbt');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+}
+else
+{
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'betagra1_exam');
+    define('DB_USER', 'betagra1_exam_u');
+    define('DB_PASSWORD', 'BetaGrades2018');
+}
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'cbt');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
 
 $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
